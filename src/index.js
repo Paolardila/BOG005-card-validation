30495b508852d2c5cea081542eb89cd308f7f06f
paola ardila <ardilavargaspaola@gmail.com>
2022-07-18 10:10:48 -0500

    cambio

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import validator from './validator.js';
 
+Validar.addEventListener("click",saludar);
+
+
+function saludar() {
+    alert("Recuerde ingresar numero de tarjeta completga");
+}
 console.log(validator);
+
+
+
