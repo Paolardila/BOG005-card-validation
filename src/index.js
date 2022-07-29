b8b4c34e05f5be238c8e403fb99c4af006670bfb
paola ardila <ardilavargaspaola@gmail.com>
2022-07-29 10:04:45 -0500

    cambios

--- src/index.js
+++ src/index.js
@@ -6,18 +6,9 @@ const boton = document.querySelector("#miBoton");
 boton.addEventListener("click", function () {
     var numberCard = document.getElementById('numerot').value;
 
-    if (numberCard && numberCard.length > 0) {
-        if (valid_credit_card(numberCard)) {
-            var firstFour = numberCard.substring(0, 4);
-            alert('Es Valida la Tarjeta:  ' + firstFour + ' ' + '****' + ' ' + '****' + ' ' + '****');
-            document.getElementById('numerot').value = '';
-        } else {
-            alert('No es valida la Tarjeta:  ' + numberCard + ' ' + '****' + ' ' + '****' + ' ' + '****');
-            document.getElementById('numerot').value = '';
-        }
-    } else {
-        alert('Por favor ingrese un numero ' + ':)');
-    }
+    var isOk= validator.isValid(numberCard)
+    console.log(isOk);
+   
 });
 
 
